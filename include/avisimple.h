4ec652f741ca26e8922ce1a51dc9541b993a2f85
koyli <david@lecomber.net>
2022-05-03 21:39:11 +0100

    don't build writeHeaderHex if arduino

--- include/avisimple.h
+++ include/avisimple.h
@@ -9,8 +9,9 @@ namespace AviFileWriter {
 
     void init_avi(const char filename[], int w, int h, int fps = 30, int format = YUYV);
     void writeHeader();
-
+#ifndef ARDUINO
     void writeHeaderHex();
+#endif
     void addFrame(const void*p, int size);
     
 }
